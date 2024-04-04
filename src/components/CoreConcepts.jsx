e2a545110fe6305dc3b1e4157522b3232be5504d
Marko Jovanovic <markojovanovic.jovanovic@gmail.com>
2024-04-04 21:32:56 +0200

    practice

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -5,7 +5,7 @@ export default function CoreConcepts() {
     return (<section id='core-concepts'>
     <h2>Core Concepts</h2>
     <ul>
-      {CORE_CONCEPTS.map((conceptItem)=>  <CoreConcept key={conceptItem.title} {...conceptItem} />)}
+      {CORE_CONCEPTS.map((conceptItem)=>  (<CoreConcept key={conceptItem.title} {...conceptItem} />))}
       
     {/* <CoreConcept 
       title={CORE_CONCEPTS[0].title}
