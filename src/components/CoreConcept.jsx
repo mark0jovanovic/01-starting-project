0417c0296c44e0f61b57a041e362afea8280e2dc
Marko Jovanovic <markojovanovic.jovanovic@gmail.com>
2024-05-08 22:29:12 +0200

    Practice

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,8 +1,9 @@
-export default function CoreConcept({image, title, description}) {
-    return( <li>
-      <img src={image} alt={title}/>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-    );
-  }
+export default function CoreConcept({ image, title, description }) {
+	return (
+		<li>
+			<img src={image} alt={title} />
+			<h3>{title}</h3>
+			<p>{description}</p>
+		</li>
+	);
+}
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,9 @@
-export default function TabButton({children, isSelected, ...props}) {
-   
-
-    return( <li>
-        <button className={isSelected ? 'active' : undefined} {...props}>{children}</button>
-        </li>)
-
+export default function TabButton({ children, isSelected, ...props }) {
+	return (
+		<li>
+			<button className={isSelected ? "active" : undefined} {...props}>
+				{children}
+			</button>
+		</li>
+	);
 }
